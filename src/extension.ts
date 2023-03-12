b41e2f2993d6f1a3747bb445adcf9f8bc27c8526
yueloi <yuelioi1210@gmail.com>
2023-03-12 14:08:46 +0800

    update

--- src/extension.ts
+++ src/extension.ts
@@ -76,21 +76,20 @@ function activate(context: { subscriptions: vscode.Disposable[] }) {
                         }
                     }
 
-                    const distFolderPath = path.join(workspaceFolder, distFolder);
+                    // 获取源文件纯名称
                     const outFileBaseName = path.basename(inputFileName, ".tsx");
                     const outFilePath = `${distFolder}/${outFileBaseName}.jsx`;
                     try {
                         // 查看有没有使用rollup
-                        const rollupConfigPath = path.join(workspaceFolder, "rollup.config-ae.js");
+                        const rollupConfigPath = path.join(workspaceFolder, "rollup.config.js");
                         if (fs.existsSync(rollupConfigPath)) {
                             const content = {
                                 input: inputFilePath,
                                 output: outFilePath,
                             };
                             const rollupPath = path.join(workspaceFolder, "node_modules", ".bin", "rollup");
-                            console.log(rollupPath);
                             fs.writeFileSync(path.join(workspaceFolder, "tsx-link.json"), JSON.stringify(content));
-                            child_process.execSync(`${rollupPath} -c ${rollupConfigPath}`, {
+                            child_process.execSync(`"${rollupPath}" -c "${rollupConfigPath}"`, {
                                 cwd: workspaceFolder,
                             });
                         } else {
@@ -103,7 +102,6 @@ function activate(context: { subscriptions: vscode.Disposable[] }) {
                     }
                     inputFilePath = path.join(workspaceFolder, outFilePath);
                 }
-                console.log(inputFilePath);
                 if (fs.existsSync(inputFilePath)) {
                     aePath = (aePath as string).indexOf(" ") === -1 ? aePath : `"${aePath}"`;
                     child_process.exec(`${aePath} -r ${inputFilePath}`, (err) => {
